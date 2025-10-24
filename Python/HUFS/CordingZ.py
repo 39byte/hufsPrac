3143181c99fe8f0363985777c745c2905e517e22
39byte <mpjw2617@gmail.com>
2025-10-24 16:19:26 +0900

    first commit

---
+++ Python/HUFS/CordingZ.py
@@ -0,0 +1,12 @@
+a = int(input()); b = int(input()); c = int(input())
+cm_list = [a, b, c]
+cm_list.sort()
+
+if cm_list[2] >= cm_list[0] + cm_list[1]:
+    print("Invalid")
+elif cm_list[0] == cm_list[1] == cm_list[2]:
+    print("Equilateral")
+elif (cm_list[0] == cm_list[1]) or (cm_list[1] == cm_list[2]):
+    print("Isosceles")
+elif cm_list[0] != cm_list[1] != cm_list[2]:
+    print("Scalene")
